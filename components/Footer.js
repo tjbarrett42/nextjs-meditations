bdf573e214886912bfc1220df1365a0021117ec3
tjbarrett42 <tjbarrett42@gmail.com>
2021-01-02 17:32:26 -0500

    About/intro, footer, container styling

---
+++ components/Footer.js
@@ -0,0 +1,37 @@
+import React, {Fragment} from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+// // Workaround for footer problems
+// const phantom = {
+//     display: 'block',
+//     padding: '20px',
+//     height: '80px',
+//     width: '100%',
+// };
+
+function Footer(){
+    return (
+        <Fragment>
+            {/*style={phantom}*/}
+            <div ></div>
+            <footer className="mt-5">
+                <Container fluid={true}>
+                    <Row className="justify-content-between p-1">
+                        <Col className="p-0 d-flex justify-content-center">
+                            Copyright © 2020 Timothy Barrett
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-between p-0">
+                        <Col className="p-0 d-flex justify-content-center">
+                            github.com/tjbarrett42
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+        </Fragment>
+    );
+}
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import Footer from "../components/Footer";
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
+
 
 import { getSortedBooksData } from '../lib/books'
 
@@ -25,33 +27,45 @@ export default function Home({ allBooksData }) {
             <title>{siteTitle}</title>
         </Head>
         <section>
-            <p>Intro</p>
+            <p>Meditations (romanized: Ta eis he'auton, 'things to one's self') is a series of personal writings by Marcus Aurelius, Roman Emperor from 161 to 180 AD, recording his private notes to himself and ideas on Stoic philosophy.</p>
         </section>
-        <section>
-            <h2>Books</h2>
-            <Container>
-                <Row>
-                    {allBooksData.map(({ id, title, subtitle }) => (
-                        <Col xs="6" lg="4" key={id}>
-                            <Button href={`/books/${id}`} className="btn-primary" block="false">{title}</Button>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-            {/*<ul>*/}
-            {/*    {allBooksData.map(({ id, title, subtitle }) => (*/}
-            {/*        <li key={id}>*/}
-            {/*            <Link href={`/books/${id}`}>*/}
-            {/*                <a>{title}</a>*/}
-            {/*            </Link>*/}
-            {/*            <br />*/}
-            {/*            <small >*/}
-            {/*                <p>{subtitle}</p>*/}
-            {/*            </small>*/}
-            {/*        </li>*/}
-            {/*    ))}*/}
-            {/*</ul>*/}
-        </section>
+
+        <Container>
+            <Row className="justify-content-center">
+                <h2>Books</h2>
+            </Row>
+            <Row>
+                {allBooksData.map(({ id, title, subtitle }) => (
+                    <Col xs="6" lg="4" key={id}>
+                        <Button href={`/books/${id}`} className="btn-primary" block="false">{title}</Button>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+        <Container>
+            <Row className="justify-content-center">
+                <h2>About</h2>
+            </Row>
+            <Row className="justify-content-center">
+                <p>http://classics.mit.edu/Antoninus/meditations.html</p>
+            </Row>
+
+        </Container>
+        {/*<ul>*/}
+        {/*    {allBooksData.map(({ id, title, subtitle }) => (*/}
+        {/*        <li key={id}>*/}
+        {/*            <Link href={`/books/${id}`}>*/}
+        {/*                <a>{title}</a>*/}
+        {/*            </Link>*/}
+        {/*            <br />*/}
+        {/*            <small >*/}
+        {/*                <p>{subtitle}</p>*/}
+        {/*            </small>*/}
+        {/*        </li>*/}
+        {/*    ))}*/}
+        {/*</ul>*/}
+
+        <Footer />
     </Layout>
   )
 }
